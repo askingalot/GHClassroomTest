55047896b3a22a95ac50a80fe2976920922e3d01
Andy Collins <askingalot@gmail.com>
2021-05-08 13:53:23 -0500

    initial commit

---
+++ Hello.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Xunit;
+using Hello;
+
+namespace Hello.Tests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void Hello_Returns_HelloWorld()
+        {
+            Assert.Equal("Hello, World!", Program.Hello());
+        }
+    }
+}
---
+++ Hello/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hello
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string Hello() {
+            return "Hello, World!";
+        }
+    }
+}
